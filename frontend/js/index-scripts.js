8318a3bd07bb4d8f2b1e475bd4dcd4e81b1fb115
deividali <damingan123@gmail.com>
2024-12-01 14:31:21 -0500

    se crea proyecto para frontend en Vercel

---
+++ frontend/js/index-scripts.js
@@ -0,0 +1,153 @@
+
+const API_URL = "http://localhost:5050/rutas"
+
+
+document.getElementById("searchButton").addEventListener('click',async(e)=>{
+    e.preventDefault();
+    const searchWord=document.getElementById('searchField').value;
+    const radios = document.getElementsByName('radioOption');
+    
+    let filterOption; 
+    for (const radio of radios) { 
+        if (radio.checked) { 
+            filterOption = radio.value; 
+            break; 
+        }}
+
+    try {
+        // Envio una peticion tipo GET al servidor para obtener los valores de rutas en pantalla acorde a la búsqueda
+
+        const response = await fetch(`${API_URL}/${searchWord}/${filterOption}`);
+        const routes = await response.json();
+        renderRoutes(routes);
+
+    } catch (error) {
+        console.log(error)
+    }
+    })
+
+
+function renderRoutes(routes){
+    const routeList = document.querySelector('#routeList')
+    routeList.innerHTML = "";
+
+    routes.forEach((route) => {
+        const routeItem = document.createElement("div");
+        routeItem.innerHTML = `
+            <span class = "route-Class" data-id = "${route.id}">
+                
+                ${route.routeName}<br>
+                ${route.motoName}
+                ${route.terrainType}
+                ${route.startPoint}
+                ${route.endPoint}
+                ${route.routeTime}
+                ${route.routeDistance}
+                ${route.userScore}${route.userName}
+            </span><br>
+            <button data-id = "${route.id}" class = "update-btn">Edit</button>
+            <button data-id = "${route.id}" class = "delete-btn">Delete</button>
+            <button data-id = "${route.id}" class = "view-btn">View</button>
+        `;
+
+        routeList.appendChild(routeItem)
+    });
+}
+
+
+// Agregar datos al fomulario
+
+// document.querySelector("#todo-form").addEventListener("submit",async(e)=>{
+//     e.preventDefault();
+
+//     const tarea = document.querySelector("#todo-input").value.trim();
+//     if(tarea==="") return;
+
+//     try {
+//         const response = await fetch(API_URL,{
+//             method: "POST",
+//             headers: {"content-type": "application/json"},
+//             body: JSON.stringify({tarea})
+//         })
+//         await response.json();
+//         fetchTodos();
+//         document.querySelector("#todo-input").value="";
+//     } catch (error) {
+//         console.log("Error al agregar datos al formulario: ",error)
+//     }
+
+// });
+
+
+// Actualizar los estados y el texto de la tarea
+
+// e = evento
+// document.querySelector("#todo-list").addEventListener("click",(e)=>{
+//     const id = e.target.dataset.id;
+//     console.log(id)
+//     if(e.target.classList.contains("update-btn")){
+//         const todoText = document.querySelector(`.todo-tarea[data-id="${id}"]`)
+//         const newText = prompt("editar tarea:",todoText.textContent)
+//         console.log(newText)
+//         if(newText&&newText.trim()!==""){
+//             console.log("condicion valida")
+//             updateTodoText(id,newText.trim())
+//         }
+//     }
+//     else if(e.target.classList.contains("view-btn")){
+//         printroute();
+//     }
+
+//     else if(e.target.type==='checkbox'){
+//         //updateTodoStatus(id,e.target.checked);
+
+//     }
+//     else if(e.target.classList.contains("delete-btn")){
+//         //deletetodo(id);
+//     }
+// });
+
+
+
+// async function updateTodoText(id,newText) {
+//     console.log("Se ingresa a funcion updateTodoText",id,newText)
+//     try {
+//         const response = await fetch(`${API_URL}/${id}`,{            
+//             method: "PUT",
+//             headers: {"content-type":"application/json"},
+//             body: JSON.stringify({"tarea":newText})
+//         })
+//         await response.json();
+//         console.log(response)
+//         fetchTodos();
+//     } catch (error) {
+//         console.log("Error al actualizar el texto: ", error)
+//     }
+// }
+
+
+// // Actualizar el estado (completado)
+
+// async function updateTodoStatus(id,completed){
+//     try {
+//         await fetch(`${API_URL}/${id}`,{
+//             method: "PUT",
+//             headers: {"content-type":"application/json"},
+//             body: JSON.stringify({completed})
+//         })
+//         fetchTodos();
+//     } catch (error) {
+//         console.log("Se ha presentado un error al actualizar el estado: ",error)
+//     }
+// }
+
+// async function deletetodo(id) {
+//     try {
+//         await fetch(`${API_URL}/${id}`,{
+//             method: "DELETE",
+//         });
+//         fetchTodos();
+//     } catch (error) {
+//         console.log("Error al borrar tarea: ", error)
+//     }
+// }
---
+++ frontend/js/map.js
@@ -0,0 +1,80 @@
+const APIURL = "http://localhost:5050/coordinate";
+const zoom=15;
+// Inicializo el mapa en cali
+var map = L.map('map').setView([3.43, -76.51], zoom);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: zoom,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+// Funcion para mostrar en el mapa la ubicacion que reciba de la base de datos
+function printroutes(x_location1,y_location1,x_location2,y_location2) {
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; OpenStreetMap contributors' 
+    }).addTo(map);
+
+    var marker_1 = L.marker([x_location1, y_location1]).addTo(map);
+    marker_1.bindPopup("<b>Hello world!</b><br>Punto 1").openPopup();
+    
+    var marker_2 = L.marker([x_location2, y_location2]).addTo(map);
+    marker_2.bindPopup("<b>Hello world!</b><br>Punto 2").openPopup();
+
+    const pointA = [x_location1, y_location1];
+    const pointB = [x_location2, y_location2];
+    
+    async function getRoute() {
+        const apiKey = '5b3ce3597851110001cf6248986864ac4a1744a0ba5e44722b513fa1';
+        const url = `https://api.openrouteservice.org/v2/directions/driving-car?api_key=${apiKey}&start=${pointA[1]},${pointA[0]}&end=${pointB[1]},${pointB[0]}`;
+    
+        try {
+            
+            const response = await fetch(url);
+            const data = await response.json();
+            // Obtener las coordenadas de la ruta
+            const coordinates = data.features[0].geometry.coordinates;
+        
+            // Convertir las coordenadas de GeoJSON a formato Leaflet (Lat, Lng)
+            const latLngs = coordinates.map(coord => [coord[1], coord[0]]);
+        
+            // Dibujar la ruta en el mapa
+            const routeLine = L.polyline(latLngs, { color: 'blue', weight: 4 }).addTo(map);
+        
+            // Ajustar el zoom para mostrar la ruta completa
+            map.fitBounds(routeLine.getBounds());
+        } catch (error) {
+            console.error("Error en la solicitud:", error);
+        }   
+    }
+    
+    // Llamar a la función para obtener y mostrar la ruta
+    getRoute();
+
+}
+
+// Creo un evento cuando escucho un click en todos los elementos con id "routeList"
+
+document.querySelector("#routeList").addEventListener("click",(e)=>{
+    const id = e.target.dataset.id;
+    console.log(id)
+    // Reviso si el que se dio click en el elemento con clase view-btn
+    if(e.target.classList.contains("view-btn")){
+        fetchRutas(id);
+    }
+
+});
+
+// Envio una peticion tipo GET al servidor para obtener las coordenadas
+async function fetchRutas(id) {
+    try {
+        const response = await fetch(`${APIURL}/${id}`);
+        const coordinate = await response.json(); 
+        printroutes(coordinate[0].xcoordinate,coordinate[0].ycoordinate,coordinate[1].xcoordinate,coordinate[1].ycoordinate);
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
